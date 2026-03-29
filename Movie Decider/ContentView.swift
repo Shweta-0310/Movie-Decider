fa659dd9f7d526b4ef3a086b993bd3ba53ba50eb
shwetaxdesign <shwetay.design@gmail.com>
2026-03-29 18:38:46 +0530

    Initial Commit

---
+++ Movie Decider/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Movie Decider
+//
+//  Created by Shweta Yadav on 29/03/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Movie Decider/Movie_DeciderApp.swift
@@ -0,0 +1,17 @@
+//
+//  Movie_DeciderApp.swift
+//  Movie Decider
+//
+//  Created by Shweta Yadav on 29/03/26.
+//
+
+import SwiftUI
+
+@main
+struct Movie_DeciderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
